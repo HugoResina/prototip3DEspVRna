0bf9bb8db02384a815848e1b677ba9ad3b7d0382
Hugo R <hugo.resina.7e7@itb.cat>
2026-01-12 12:43:27 +0100

    edit output try

--- Assets/MoveNPC.cs
+++ Assets/MoveNPC.cs
@@ -21,11 +21,11 @@ public class MoveNPC : MonoBehaviour
         {
             Debug.LogError("nav mesh agent component not attached");
         }
-        //else
-        //{
-        //    SetDestination(destination1);
-        //}
-        
+        else
+        {
+            SetDestination(destination1);
+        }
+
     }
     private void OnEnable()
     {

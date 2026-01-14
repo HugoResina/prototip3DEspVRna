df1c93ba9e86ae1abc863c2f2bd6e798f227e874
Hugo R <hugo.resina.7e7@itb.cat>
2026-01-14 10:08:40 +0100

    save progress

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 public class Interactable : MonoBehaviour
 {
     public string interactionPrompt = "Prem 'E' per interactuar";
+    public Canvas TalkCanvas;
 
     public virtual void Interact()
     {
         Debug.Log("Interacting with " + gameObject.name);
+        //activate canvas to talk
+        TalkCanvas.gameObject.SetActive(true);
     }
 
 }

ba7c052e8c3d910556e69ef8d843b9f9a15fb5b0
Hugo Resina <hugo.resina.7e7@itb.cat>
2026-03-19 13:26:34 +0100

    ambulance follow

--- Assets/Scripts/StartCall.cs
+++ Assets/Scripts/StartCall.cs
@@ -4,6 +4,7 @@ public class StartCall : MonoBehaviour
 {
     private void Start()
     {
+        
         InteractablePerson IPer = GetComponent<InteractablePerson>();
         IPer.Interact(gameObject);
     }
